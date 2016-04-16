efea7b771e86cbf8d171e3c2ea09abb92688f111
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-04-16 12:44:08 -0400

    Initial Commit

---
+++ fitiquick/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fitiquick
+//
+//  Created by Nikhil Verma on 16/04/16.
+//  Copyright © 2016 Nikhil Verma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
